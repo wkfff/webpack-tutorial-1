e93f70bb02f1744c3884d43a7c41a57195d09d20
Viktor Pyskunov <vpyskunov@gmail.com>
2020-10-30 10:04:33 +0200

    Creating Navigation Bar

--- dashboard/webpack.production.config.js
+++ dashboard/webpack.production.config.js
@@ -30,6 +30,12 @@ module.exports = {
                         presets: [ '@babel/env' ],
                     }
                 }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
+                ]
             }
         ]
     },
